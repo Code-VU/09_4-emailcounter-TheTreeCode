5ac61e960c345ef06bbac28f41e55ffc81dbe1c3
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-04-02 14:00:42 -0500

    cleaned up code by taking out the loop and using one line with dict(zip(list1, list2)) functions.

--- map2listsToDictionary.py
+++ map2listsToDictionary.py
@@ -1,9 +1,5 @@
-l1 = ['a','b','c']
-l2 = [1,2,3]
-d1 = dict()
-counter = 0
-for i in l1:
-    d1.setdefault(i, l2[counter])
-    counter += 1
+letters = ['a','b','c']
+numbers = [1,2,3]
+d1 = dict(zip(letters,numbers))
 
 print(d1)
