c30d6aa197669301647f8a2eaf2335044965349f
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-04-02 14:20:00 -0500

    Updated code to write dictionary to new file to be used on next bonus exercise.

--- map2listsToDictionary.py
+++ map2listsToDictionary.py
@@ -1,5 +1,7 @@
 letters = ['a','b','c']
 numbers = [1,2,3]
 d1 = dict(zip(letters,numbers))
-
+filetocreate = open(input('Enter a file name that you would like to add this dictionary to: '), 'w')
+filetocreate.write(str(d1))
+filetocreate.close()
 print(d1)
---
+++ max_and_min_in_dict.py
@@ -0,0 +1,7 @@
+fhand = open('test_file_for_next_bonus_exercise.txt')
+d1 = {}
+for i in fhand:
+    d1 = i
+print(d1)
+for i in d1:
+    print(i)
